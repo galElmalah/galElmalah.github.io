5c980a1fce778c47fd68d246acfdf88f36f088d5
Gal Elmalah <galisrael8914@gmail.com>
2018-04-20 10:12:28 +0300

    added smooth scrolling and the bottom menu

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,35 @@
       });
     });
   }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+// changing the navbar color on scroll
+window.addEventListener('scroll', ()=>{
+  const contactHeight = document.getElementById('contact').offsetTop;  
+  if(window.pageYOffset > contactHeight -50 ){
+    const navbar = document.getElementById('main-navigation');
+    if(!navbar.classList.contains('lower-nav')){
+      navbar.classList.add('lower-nav');
+      const items = document.getElementsByClassName('navbar-item');
+      Array.from(items).forEach(item =>{
+        item.classList.add('lower-nav-items')});
+    }
+  } else {
+    const navbar = document.getElementById('main-navigation');
+    if(navbar.classList.contains('lower-nav')){
+      navbar.classList.remove('lower-nav');
+      const items = document.getElementsByClassName('navbar-item');
+      Array.from(items).forEach(item =>{
+      item.classList.remove('lower-nav-items');
+    })
+  }
+  }
+})
